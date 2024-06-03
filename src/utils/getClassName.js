3f168eb87d1a97143a403b17dc353152414bdacc
mimgusalad <dakjijisalad@gmail.com>
2024-06-04 07:49:53 +0900

    file name changed

---
+++ src/utils/getClassName.js
@@ -0,0 +1,14 @@
+import { isMobile, isTablet } from "react-device-detect";
+const getClassName = () => {
+  let classNames = "card";
+  if (isMobile) {
+    classNames = "mobile-" + classNames;
+  } else if (isTablet) {
+    classNames = "tablet-" + classNames;
+  } else {
+    classNames = "desktop-" + classNames;
+  }
+  return classNames;
+};
+
+export default getClassName;
