15340d7a52dda98deb3a2389dee9afba9f274891
mimgusalad <dakjijisalad@gmail.com>
2024-06-22 17:48:14 +0900

    api 주소 수정

--- src/utils/getSubtitles.js
+++ src/utils/getSubtitles.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 export async function getSubtitles(videoId, langCode) {
   try {
-    const response = await axios.get("http://localhost:8080/file/subtitle", {
-      params: { videoId, langCode },
-    });
+    const response = await axios.get(
+      "https://api.plave-subtitles.com/file/subtitle",
+      {
+        params: { videoId, langCode },
+      }
+    );
     const subtitles = response.data;
     const subtitleHashTable = {};
     for (let i = 0; i < subtitles.length; i++) {
